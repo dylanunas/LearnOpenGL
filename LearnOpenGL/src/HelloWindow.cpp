1f5bce64c617109007a01d5db5ec215bdf79f38e
Dylan Unas <dylanunas@gmail.com>
2023-12-22 17:48:22 -0500

    Added glfw and glad initialization

---
+++ LearnOpenGL/src/HelloWindow.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+int main() {
+
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	
+	GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
+	if (window == NULL) {
+		std::cout << "ERROR: Failed to create GLFW window!" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+		std::cout << "ERROR: Failed to initialize GLAD!" << std::endl;
+		return -1;
+	}
+
+
+
+	return 0;
+}
--- LearnOpenGL/src/test.cpp
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-#include "glad/glad.h"
-
-int main() {
-	std::cout << "Hello World!" << std::endl;
-	return 0;
-}
