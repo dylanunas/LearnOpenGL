f16ea46f39a14d3a032941e4fc484418a222006f
Dylan Unas <dylanunas@gmail.com>
2023-12-26 23:11:55 -0500

    added another vertex attribute, which are the colours for each vertex

--- LearnOpenGL/src/HelloWindow.cpp
+++ LearnOpenGL/src/HelloWindow.cpp
@@ -11,15 +11,20 @@ void processInput(GLFWwindow* window);
 
 const char* vertexShaderSource = "#version 330 core\n"
 	"layout (location = 0) in vec3 aPos;\n"
+	"layout (location = 1) in vec3 aColor;\n"
+
+	"out vec3 ourColor;\n"
+
 	"void main() {\n"
 	"    gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+	"    ourColor = aColor;\n"
 	"}\n\0";
 
 const char* fragmentShaderSource = "#version 330 core\n"
 	"out vec4 FragColor;\n"
-	"uniform vec4 ourColor;\n"
+	"in vec3 ourColor;\n"
 	"void main() {\n"
-	"FragColor = ourColor;\n"
+	"FragColor = vec4(ourColor, 1.0);\n"
 	"}\n\0";
 
 int main() {
@@ -102,9 +107,10 @@ int main() {
 
 	// 3D coords for a triangle
 	float triangleVertices[] = {
-		-0.5f, -0.5f, 0.0f, // bottom left
-		 0.5f, -0.5f, 0.0f, // bottom right
-		 0.0f,  0.5f, 0.0f  // top
+		// positions		 // colours
+		-0.5f, -0.5f, 0.0f,  1.0f, 0.0f, 0.0f,// bottom left
+		 0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,// bottom right
+		 0.0f,  0.5f, 0.0f,  0.0f, 0.0f, 1.0f// top
 	};
 
 	unsigned int EBO, VAO, VBO;
@@ -117,8 +123,13 @@ int main() {
 	// bind the newly created buffer to a VBO then copy the vertex data onto the buffer's memory
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(triangleVertices), triangleVertices, GL_STATIC_DRAW);
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+
+	// position attribute
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
+	// color attribute
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+	glEnableVertexAttribArray(1);
 
 	// the application should keep running until we explicitly told it to stop
 	while (!glfwWindowShouldClose(window)) {
