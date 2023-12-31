2ed03811675a8a67cac8b1c12558df091504e116
Dylan Unas <dylanunas@gmail.com>
2023-12-30 22:53:49 -0500

    Loaded my first texture, though I need to change the triangle into a rectangle

--- LearnOpenGL/src/LearnOpenGL.cpp
+++ LearnOpenGL/src/LearnOpenGL.cpp
@@ -3,6 +3,10 @@
 
 #include <iostream>
 
+// include the stb_image.h file
+#define STB_IMAGE_IMPLEMENTATION
+#include <stb/stb_image.h>
+
 #include "Shader.h"
 
 // window size should change when user resizes the screen
@@ -37,16 +41,16 @@ int main() {
 	
 	// shader program
 	Shader shaderProgram(
-		"C:/PersonalProjects/LearnOpenGL/LearnOpenGL/src/ShaderPrograms/vertexShaderSource.vert", 
-		"C:/PersonalProjects/LearnOpenGL/LearnOpenGL/src/ShaderPrograms/fragmentShaderSource.frag"
+		"src/ShaderPrograms/vertexShaderSource.vert",
+		"src/ShaderPrograms/fragmentShaderSource.frag"
 	);
 
 	// 3D coords for a triangle
 	float triangleVertices[] = {
-		// positions		 // colours
-		-0.5f, -0.5f, 0.0f,  1.0f, 0.0f, 0.0f,// bottom left
-		 0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,// bottom right
-		 0.0f,  0.5f, 0.0f,  0.0f, 0.0f, 1.0f// top
+		// positions		 // colours         // texture coords
+		-0.5f, -0.5f, 0.0f,  1.0f, 0.0f, 0.0f,  0.0f, 0.0f,   // bottom left
+		 0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,  1.0f, 0.0f,   // bottom right
+		 0.0f,  0.5f, 0.0f,  0.0f, 0.0f, 1.0f,  0.5f, 1.0f    // top
 	};
 
 	unsigned int EBO, VAO, VBO;
@@ -61,13 +65,40 @@ int main() {
 	glBufferData(GL_ARRAY_BUFFER, sizeof(triangleVertices), triangleVertices, GL_STATIC_DRAW);
 
 	// position attribute
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
 	// color attribute
-	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
 	glEnableVertexAttribArray(1);
+	// texture coordinates attribute
+	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
+	glEnableVertexAttribArray(2);
 
-	// the application should keep running until we explicitly told it to stop
+	// ==================== creating and loading a texture =======================
+	unsigned int texture;
+	glGenTextures(1, &texture);
+	glBindTexture(GL_TEXTURE_2D, texture);
+
+	// set texture wrapping and filtering options
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+	// load the texture from an image
+	int width, height, nrChannels;
+	unsigned char* data = stbi_load("textures/container.jpg", &width, &height, &nrChannels, 0);
+	// check if the data has loaded successfully
+	if (data) {
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+		glGenerateMipmap(GL_TEXTURE_2D);
+	}
+	else {
+		std::cout << "ERROR: Failed to load the texture!" << std::endl;
+	}
+	stbi_image_free(data);
+
+	// ============================== render loop ================================
 	while (!glfwWindowShouldClose(window)) {
 		// listen to escape key being pressed to close the GLFW window
 		processInput(window);
@@ -81,13 +112,11 @@ int main() {
 		// use the program before updating the uniform value
 		shaderProgram.use();
 
+		// apply the texture
+		glBindTexture(GL_TEXTURE_2D, texture);
 		// draw the triangle
 		glBindVertexArray(VAO);
 		glDrawArrays(GL_TRIANGLES, 0, 3);
-
-		// move the triangle to the right
-		float xLocationOffset = 0.5f;
-		shaderProgram.setFloat("xOffset", xLocationOffset);
 	
 		// listen to events
 		glfwSwapBuffers(window);
--- LearnOpenGL/src/ShaderPrograms/vertexShaderSource.vert
+++ LearnOpenGL/src/ShaderPrograms/vertexShaderSource.vert
@@ -1,14 +1,15 @@
 #version 330 core
 layout (location = 0) in vec3 aPos;
 layout (location = 1) in vec3 aColor;
+layout (location = 2) in vec2 aTexCoord;
 
 out vec3 ourColor;
-out vec3 outPos;
+out vec2 texCoord;
 
 uniform float xOffset = 0;
 
 void main() {
 	gl_Position = vec4(aPos.x + xOffset, -aPos.y, aPos.z, 1.0);
 	ourColor = aColor;
-	outPos = aPos;
+	texCoord = aTexCoord;
 }
